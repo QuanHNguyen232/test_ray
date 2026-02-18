67f00d9791d6170370e12060cdd245382378079b
QuanHNguyen232 <quan.nh2002@gmail.com>
2026-02-18 04:12:56 -0500

    update simple test

---
+++ README.md
@@ -0,0 +1,23 @@
+# Launch Ray job
+1. Set env variable
+```bash
+export RAY_API_SERVER_ADDRESS=
+```
+1. Steps:
+```bash
+git clone https://github.com/QuanHNguyen232/test_ray.git
+cd test_ray/src
+
+# install dependencies
+uv sync
+# OR
+pip install -r requirements.txt
+
+# back to test_ray/
+cd ..
+
+# using uv
+ray job submit --no-wait --working-dir . -- uv run --directory src main_src.py
+# OR using python
+ray job submit --no-wait --working-dir . --runtime-env-json='{"pip": "./src/requirements.txt"}' -- python src/main_src.py
+```
--- src/main_src.py
+++ src/main_src.py
@@ -1,11 +1,38 @@
-def main():
-    print("Hello from test-ray/src!")
-    try:
-        import emoji
-        print(emoji.emojize("This is a test :thumbs_up:"))
-    except ImportError:
-        print("Emoji library not found. Please install it to see emojis.")
+print("Hello from test-ray/src!")
 
+try:
+    import ray
+    ray.init()
+    print("ray version:", ray.__version__)
+    print("cluster_resources:", ray.cluster_resources())
+    print("available_resources:", ray.available_resources())    
+except Exception as e:
+    print("Error initializing Ray:", e)
 
-if __name__ == "__main__":
-    main()
+try:
+    import torch
+    print("torch version:", torch.__version__)
+    print("torch cuda is available:", torch.cuda.is_available())
+except Exception as e:
+    print("Error importing torch:", e)
+    
+try:
+    import emoji
+    print(emoji.emojize("This is a test :thumbs_up:"))
+except ImportError:
+    print("Emoji library not found. Please install it to see emojis.")
+    
+@ray.remote
+def add_one(a):
+    return a + 1
+try:
+    obj_ref = add_one.remote(0)
+    for i in range(1000):
+        obj_ref = add_one.remote(obj_ref)
+    print(ray.get(obj_ref))
+except Exception as e:
+    print("Error running Ray remote func:", e)
+    
+
+if ray.is_initialized():
+    ray.shutdown()
